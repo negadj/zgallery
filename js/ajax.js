2bb8270ba856fc2e48b7a8992780d8a977fbeb30
zoldatoff <zoldatoff@db496e8e-af76-11de-9d1a-45ab25924541>
2009-12-13 22:07:42 +0000

    New version of zGallery - first step to version 2.0. Front-face changed

---
+++ js/ajax.js
@@ -0,0 +1,149 @@
+/**
+ * @author zoldatoff
+ */
+
+var ajaxPath = 'http://127.0.0.1/zgallery/php/upload.php';
+
+$(document).ready(function(){
+    $.ajaxSetup({
+        url: ajaxPath,
+        timeout: 10000,
+        contentType: 'application/json',
+        error: ajaxError,
+        complete: ajaxComplete,
+        beforeSend: ajaxSend
+    });
+    getCategories();
+    $('header img').click(getCategories);
+});
+
+function getCategories(){
+    $.getJSON(ajaxPath, {
+        object: 'categories'
+    }, fillCategories);
+}
+
+function getAlbums(category_id){
+    $.getJSON(ajaxPath, {
+        object: 'albums',
+        category_id: category_id
+    }, fillAlbums);
+}
+
+function getImages(album_id){
+    $.getJSON(ajaxPath, {
+        object: 'images',
+        album_id: album_id
+    }, fillImages);
+}
+
+function fillCategories(jsonData){
+    var theList = jsonData.objectlist.category_list;
+    var nItems = theList.length;
+    var myLi;
+    
+    $("#navUL").empty();
+    
+    for (var i = 1; i < nItems; i++) {
+        myLi = $('<li>').addClass('navLI').append(theList[i].name);
+        myLi.data('json', theList[i]);
+        myLi.click(function(){
+            getAlbums($(this).data('json').category_id);
+        });
+        $("#navUL").append(myLi);
+    }
+}
+
+function fillAlbums(jsonData){
+    var theList = jsonData.objectlist.album_list;
+    var nItems = theList.length;
+    var myLi, myImg, myDiv;
+    
+    $("#albUL").empty();
+    
+    for (var i = 0; i < nItems; i++) {
+        myLi = $('<li>').addClass('albLI').addClass('loadingdiv');
+        myLi.data('json', theList[i]).data('n', i);
+		
+		myImg = $('<img>').attr('src', theList[i].image.thumb_src).attr('alt',theList[i].image.name);
+		myDiv = $('<div>').addClass('albTitle').append(theList[i].name);
+
+		myLi.append(myImg).append(myDiv);
+		
+        myLi.click(function(){
+			var $albUL = $('#albUL');
+            getImages($(this).data('json').album_id);
+			
+			// Scroll the albums list to center current album
+			var steps =  Math.round($albUL.data('visibleLength')/2) - $albUL.data('currentItem') - $(this).data('n');
+			$albUL.scrollVertically(steps, $albUL.data('scrollDelta'));
+			
+			$('#albUL img').removeClass('active');
+			$(this).children('img').addClass('active');
+        });
+        $("#albUL").append(myLi);
+    }
+	
+	initAlbumsScroll();
+}
+
+function fillImages(jsonData){
+	var theList = jsonData.objectlist.image_list;
+    var nItems = theList.length;
+    var myLi;
+    
+    $("#imgUL").empty();
+    
+    for (var i = 1; i < nItems; i++) {
+        myLi = $('<li>').addClass('imgLI').addClass('loadingdiv');
+        myLi.data('json', theList[i]).data('n', i);
+		
+		myImg = $('<img>').attr('src', theList[i].thumb_src).attr('alt',theList[i].name);
+		
+        myLi.click(function(){
+			var jsonData = $(this).data('json');
+			var $imgMain = $('#imgMain');
+			var $imgUL = $('#imgUL');
+			
+			$imgMain.toggleImage(jsonData.norm_src);
+			$imgMain.data('json', jsonData).data('n', i);
+			
+			// Scroll the image list to center current image
+			var steps = Math.round($imgUL.data('visibleLength')/2) - $imgUL.data('currentItem') - $(this).data('n');
+			$imgUL.scrollHorisontally(steps, $imgUL.data('scrollDelta'));
+			
+			$('#imgUL img').removeClass('active');
+			$(this).children('img').addClass('active');
+        });
+		
+		myLi.append(myImg);
+        $("#imgUL").append(myLi);
+    }
+	
+	initImagesScroll();
+}
+
+$.fn.toggleImage = function(newSrc, duration) {
+	var d = duration ? duration : 500;
+	
+	$(this).parent().addClass('loadingdiv');
+	$(this).fadeOut(d, function(){
+		$(this).attr('src', newSrc).fadeIn(d);
+		$(this).parent().removeClass('loadingdiv');
+	});
+}
+
+function ajaxError(XMLHttpRequest, textStatus, errorThrown){
+    //console.info(XMLHttpRequest);
+    //console.info(textStatus);
+    //console.info(errorThrown);
+    $('header img').attr('src', 'icons/main_.png');
+}
+
+function ajaxComplete(XMLHttpRequest, textStatus){
+    $('header img').attr('src', 'icons/main_.png');
+}
+
+function ajaxSend(XMLHttpRequest){
+    $('header img').attr('src', 'css/loader_.gif');
+}
---
+++ js/scroll.js
@@ -0,0 +1,143 @@
+function initAlbumsScroll(){
+	/* Init vertical scroll for albums */
+    var $albUL = $("#albUL");
+    var scrollHeight = prepareUL($albUL, 'v');
+    
+    $("#albUp img").click(function(){
+        $albUL.scrollVertically(-1, scrollHeight);
+    });
+    
+    $("#albDown img").click(function(){
+        $albUL.scrollVertically(1, scrollHeight);
+    });
+	
+	$albUL.scrollVertically(0, 0);
+	
+	$("#albUL img").each(function(){
+        return $(this).load(function(){
+            $(this).parent().removeClass('loadingdiv');
+            return $(this);
+        });
+    });
+}
+
+function initImagesScroll(){
+	// Init horisontal scroll for images
+    var $imgUL = $("#imgUL");
+    var scrollWidth = prepareUL($imgUL, 'h');
+    
+    $("#imgLeft img").click(function(){
+        $imgUL.scrollHorisontally(-1, scrollWidth);
+    });
+    
+    $("#imgRight img").click(function(){
+        $imgUL.scrollHorisontally(1, scrollWidth);
+    });
+	
+	$imgUL.scrollHorisontally(0, 0);
+	
+    $("#imgUL img").each(function(){
+        return $(this).load(function(){
+            $(this).parent().removeClass('loadingdiv');
+            return $(this);
+        });
+    });
+}
+
+/* Determine scrolling parameters */
+function prepareUL($ul, direction){
+	var scrollDelta, visibleLength;
+	if (direction === 'v') {
+		scrollDelta = $ul.children(":first-child").fullHeight();
+		visibleLength = Math.floor($ul.parent().innerHeight() / scrollDelta);
+	}
+	else {
+		scrollDelta = $ul.children(":first-child").fullWidth();
+		visibleLength = Math.floor($ul.parent().innerWidth() / scrollDelta);
+	}
+	
+	$ul.data("length", $ul.children().length);
+	$ul.data("visibleLength", visibleLength);
+    $ul.data("currentItem", 0);
+	
+	$ul.data('scrollDelta', scrollDelta);
+	
+	return scrollDelta;
+}
+
+$.fn.fullHeight = function(){
+    return parseInt($(this).outerHeight(), 10) +
+    parseInt($(this).css("margin-top"), 10) +
+    parseInt($(this).css("margin-bottom"), 10);
+};
+
+$.fn.fullWidth = function(){
+    return parseInt($(this).outerWidth(), 10) +
+    parseInt($(this).css("margin-left"), 10) +
+    parseInt($(this).css("margin-right"), 10);
+};
+
+$.fn.scroll = function(steps, scrollSize, direction){
+    var $t = $(this);
+    var length = $t.data("length");
+    var visibleLength = $t.data("visibleLength");
+    var currentItem = $t.data("currentItem");
+    
+    if (currentItem <= visibleLength - length && steps < 0) {
+        return $t;
+    }
+    
+    if (currentItem >= 0 && steps > 0) {
+        return $t;
+    }
+    
+    $t.data("currentItem", currentItem + steps);
+    
+    if (direction === 'v') {
+        return $t.stop().animate({
+            "top": "+=" + scrollSize * steps + "px"
+        });
+    }
+    
+    if (direction === 'h') {
+        return $t.stop().animate({
+            "left": "+=" + scrollSize * steps + "px"
+        });
+    }
+};
+
+/* Determine wether to change scrolling icons or not */
+function changeIcon($ul, direction, $direction1, $direction2) {
+	var length = $ul.data("length");
+    var visibleLength = $ul.data("visibleLength");
+    var currentItem = $ul.data("currentItem");
+	
+	var src1 = (direction === 'v') ? "icons/up.png" : "icons/left.png";
+	var src2 = (direction === 'v') ? "icons/down.png" : "icons/right.png";
+    
+    if (currentItem <= visibleLength - length) {
+		$direction1.hide();
+    }
+    else {
+		$direction1.show();
+    }
+	
+    if (currentItem >= 0) {
+		$direction2.hide();
+    }
+    else {
+		$direction2.show();
+    }
+	
+	return $ul;
+}
+
+$.fn.scrollVertically = function(steps, scrollHeight){
+	var $t = $(this).scroll(steps, scrollHeight, 'v');
+	return changeIcon($t, 'v', $('#albUp img'), $("#albDown img"));
+};
+
+$.fn.scrollHorisontally = function(steps, scrollWidth){
+    var $t = $(this).scroll(steps, scrollWidth, 'h');
+	return changeIcon($t, 'h', $('#imgLeft img'), $("#imgRight img"));
+};
